0226a63f4007af8edd0cf73778d407809460e1bd
vetalikpain <vitalikmoshkaa@gmail.com>
2022-06-22 23:22:17 +0300

    footer develop

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import * as styles from "./styles.module.scss"
 import {Link} from "gatsby";
+import YoutubeIcon from "../../assets/YoutubeIcon";
+import InstagramIcon from "../../assets/InstagramIcon";
+import TelegramIcon from "../../assets/TelegramIcon";
+
+import * as styles from "./styles.module.scss"
 
 const Footer = () => {
     return (
-        <div className={styles.footer}>
-            <ul className={styles.footer_nav}>
-                <Link to={`/About`}>About</Link>
-                <Link to={`/Help`}>Help</Link>
-                <Link to={`/FAQ`}>FAQ</Link>
-            </ul>
-        </div>
+        <footer>
+            <nav>
+                <li><Link to={`/About`}>about</Link></li>
+                <li><Link to={`/Help`}>help</Link></li>
+                <li><Link to={`/FAQ`}>faq</Link></li>
+            </nav>
+            <div className={styles.footer_contacts}>
+                <div className={styles.footer_email}>
+                    <a href="mailto:info.daristil@gmail.com">info.daristil@gmail.com</a>
+                </div>
+            </div>
+            <div className={styles.footer_socials}>
+                <ul>
+                    <InstagramIcon />
+                    <TelegramIcon />
+                    <YoutubeIcon />
+                </ul>
+            </div>
+        </footer>
     );
 };
 
